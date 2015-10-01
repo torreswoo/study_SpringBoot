f8b4ff14307a926191c825b27c5c9ad59eae06ce
torreswoo <torreswoo@gmail.com>
2015-10-01 23:55:10 +0900

    [#01 Controller] example Hello World

---
+++ src/main/java/com/skplanet/test/TestController.java
@@ -0,0 +1,16 @@
+package com.skplanet.test;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+	
+	@RequestMapping("/")
+	String root(){
+		return "Hello World!";
+	}
+
+}
